95d95ca3bd0501c316b01822457859d5f3bcf421
Denisov-Egor <129548353+Denisov-Egor@users.noreply.github.com>
2026-03-08 21:33:29 +0300

    18

---
+++ OAiP/WinFormsApp7/WinFormsApp7/Form1.cs
@@ -0,0 +1,68 @@
+using System.Runtime.CompilerServices;
+using static System.Windows.Forms.DataFormats;
+
+namespace WinFormsApp7
+{
+
+
+    public partial class MatchingGame : Form
+    {
+        // Use this Random object to choose random icons for the squares
+        Random random = new Random();
+
+        // Each of these letters is an interesting icon
+        // in the Webdings font,
+        // and each icon appears twice in this list
+        List<string> icons = new List<string>()
+        {
+            "b", "b", "v", "v", "w", "w", "z", "z",
+            "!", "!", "N", "N", ",", ",", "k", "k",
+        };
+
+        /// <summary>
+        /// Assign each icon from the list of icons to a random square
+        /// </summary>
+        private void AssignIconsToSquares()
+        {
+            // The TableLayoutPanel has 16 labels,
+            // and the icon list has 16 icons,
+            // so an icon is pulled at random from the list
+            // and added to each label
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
+        public MatchingGame()
+        {
+            InitializeComponent();
+
+            AssignIconsToSquares();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+                // If the clicked label is black, the player clicked
+                // an icon that's already been revealed --
+                // ignore the click
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+                clickedLabel.ForeColor = Color.Black;
+            }
+
+        }
+    }
+}
