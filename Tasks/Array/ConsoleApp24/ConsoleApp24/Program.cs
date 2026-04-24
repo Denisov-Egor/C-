64ab87d881b3e226a2e5d72091ec493c0afb7d93
Denisov-Egor <129548353+Denisov-Egor@users.noreply.github.com>
2026-04-25 00:26:39 +0300

    27

---
+++ Tasks/Array/ConsoleApp24/ConsoleApp24/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 5, 2, 9, 1, 5, 6 };
+
+        arr.Sort((x, y) => y.CompareTo(x));
+
+        foreach (var Arr in arr)
+        {
+            Console.WriteLine(Arr);
+        }
+    }
+}
---
+++ Tasks/Array/ConsoleApp25/ConsoleApp25/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = Convert.ToInt32 (Console.ReadLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+
+           
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] % 2 == 0)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] % 2 != 0)
+            {
+                    Console.WriteLine("dsds");
+
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
